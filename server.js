c0955ad91813135e76e0669f935fdb753ae403e9
sarahlje <sarahjinee@hotmail.com>
2025-05-19 03:57:52 +0000

    change to ejs

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
+// Needed for dotenv
+require("dotenv").config();
+
+// Needed for Express
+var express = require('express')
+var axios = require('axios');
+var app = express()
+
+// Needed for EJS
+app.set('view engine', 'ejs');
+
+// Needed for public directory
+app.use(express.static(__dirname + '/public'));
+
+// Needed for parsing form data
+app.use(express.json());       
+app.use(express.urlencoded({extended: true}));
+
+// Needed for Prisma to connect to database
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient();
+
 const express = require('express');
 const cors = require('cors');
 const workoutRoutes = require('./routes/workout');
